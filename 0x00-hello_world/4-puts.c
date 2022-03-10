a7b5f9eccbf779caa7a38fbcdd02e108aba82be1
holinga <herbertolinga@gmail.com>
2022-03-10 08:58:53 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Print Programming is like buildng a multilingual puzzle
+ * Return: 0 if successful,otherwise not 0 unsuccessful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
