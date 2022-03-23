88d9703203d06068aa1a0cd6242409c6f540a23a
holinga <herbertolinga@gmail.com>
2022-03-23 08:45:58 -0700

    betty fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,25 @@
-#include "main.h"
+#include <stdio.h>
+/* main - main program function */
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+/*
+*
+* write your line of code here
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 
-/**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
- */
-char * _strcat(char * dest, char * src) {
-  int i, j;
-
-  i = 0;
-  j = 0;
-
-  while (dest[i] != '\0')
-    i++;
-
-  while (src[j] != '\0') {
-    dest[i] = src[j];
-    j++;
-    i++;
-  }
-
-  dest[i] = '\0';
-
-  return (dest);
 }
+
